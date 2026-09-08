968586ecd638e268cdc396c5c8984da5a9bf9a2f
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2026-09-08 10:43:03 +0530

    Time: 0 ms (100%), Space: 22 MB (15.34%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,25 +2,48 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
-        vector<vector<int>> mergedIntervals;
+        if(n==0)
+            return {newInterval};
 
         int idx = 0;
-        while(idx < n && intervals[idx][1] < newInterval[0]){
-            mergedIntervals.push_back(intervals[idx]);
-            idx++;
+
+        int low = 0, high = n-1, startIdx = n;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(intervals[mid][1] >= newInterval[0]){
+                startIdx = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
         }
 
-        while(idx<n && intervals[idx][0] <= newInterval[1]){
-            newInterval[0] = min(newInterval[0], intervals[idx][0]);
-            newInterval[1] = max(newInterval[1], intervals[idx][1]);
-            idx++;
-        }
-        mergedIntervals.push_back(newInterval);
 
-        while(idx < n){
-            mergedIntervals.push_back(intervals[idx]);
-            idx++;
+        low = 0, high=n-1;
+        int endIdx = -1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(intervals[mid][0] <= newInterval[1]){
+                endIdx = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
         }
-        return mergedIntervals;
+
+
+        if(startIdx <= endIdx){
+            newInterval[0] = min(intervals[startIdx][0], newInterval[0]);
+            newInterval[1] = max(intervals[endIdx][1], newInterval[1]);
+        }
+
+        intervals.erase(intervals.begin() + startIdx, intervals.begin() + endIdx + 1);
+        intervals.insert(intervals.begin() + startIdx, newInterval);
+
+
+        return intervals;
     }
 };
