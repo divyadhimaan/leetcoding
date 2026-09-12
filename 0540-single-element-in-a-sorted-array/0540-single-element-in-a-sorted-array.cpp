ed6d21cefa9ff6752e9e7d77f929a8fb00b8c0cd
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2026-09-12 23:36:17 +0530

    Time: 0 ms (100%), Space: 26.4 MB (8.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,9 +6,31 @@ public:
         if(n==1)
             return nums[0];
 
-        for(int i=0;i<n;i += 2){
-            if(nums[i] != nums[i+1])
-                return nums[i];
+        
+
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+
+        int low = 1, high = n-2;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+                return nums[mid];
+            if(mid%2 == 0)
+            {
+                if(nums[mid] == nums[mid+1])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }else{
+                if(nums[mid]==nums[mid-1])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
         }
         return -1;
     }
