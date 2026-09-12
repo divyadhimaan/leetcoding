117e2e503ffbee525c41d05ec8787e5e594df15a
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2026-09-12 17:02:14 +0530

    Time: 70 ms (77.98%), Space: 88.9 MB (67.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -6,21 +6,7 @@ public:
         for(int num: nums)
             st.insert(num);
 
-        int idx = 0, length = 0;
-
-        // for(;idx < nums.size(); idx++){
-        //     if(!st.count(nums[idx]-1)){
-        //         int streak = 1;
-        //         int currEle = nums[idx];
-                                
-        //         while(st.count(currEle + 1))
-        //         {
-        //             streak++; // 4
-        //             currEle++; //4  
-        //         }
-        //         length = max(length, streak); 
-        //     }
-        // }
+        int length = 0;
 
         for(int num: st){
             if(!st.count(num-1)){
