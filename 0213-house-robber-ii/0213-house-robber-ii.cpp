4972f4e3165f6132084dc7daba042c48ad047cfb
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2026-09-14 16:00:56 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -8,16 +8,23 @@ public:
         if(n==1)
             return nums[start];
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[start];
-        dp[1] = max(nums[start], nums[start+1]);
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[start];
+        // dp[1] = max(nums[start], nums[start+1]);
+        int prev2 = nums[start];
+        int prev = max(nums[start], nums[start+1]);
+        int curr;
 
 
         for(int i=2; i<n;i++){
-            dp[i] = max(dp[i-1], nums[start+i] + dp[i-2]);
+            curr = max(prev, nums[start+i] + prev2);
+            // dp[i] = max(dp[i-1], nums[start+i] + dp[i-2]);
+            prev2 = prev;
+            prev = curr;
+
         }
 
-        return dp[n-1];
+        return prev;
 
         
     }
