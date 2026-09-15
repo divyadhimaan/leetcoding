a5c566b23e0f9788a700decea9c8310bb61d3671
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2026-09-15 09:56:42 +0530

    Time: 0 ms (100%), Space: 19.1 MB (97.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &adjList, int currNode, vector<bool> &visited){
+    void dfs(vector<vector<int>> &isConnected, int currNode, vector<bool> &visited){
         visited[currNode] = true;
 
-        for(int nextNode: adjList[currNode])
+        for(int nextNode=0;nextNode<isConnected.size();nextNode++)
         {
-            if(!visited[nextNode])
-                dfs(adjList, nextNode, visited);
+            if(!visited[nextNode] && isConnected[currNode][nextNode])
+                dfs(isConnected, nextNode, visited);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adjList(n);
 
-        //Step 1: Create adjacency list
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j] == 1)
-                {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
-            }
-        }
 
         vector<bool> visited(n, false);
         int provinces = 0;
+
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(adjList, i, visited);
+                dfs(isConnected, i, visited);
                 provinces++;
             }
         }
